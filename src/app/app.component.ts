c22af011b7f4e8dcd4e02814e3206e268f7b92bc
Tiennui <tiennvspkt@gmail.com>
2017-06-23 10:49:03 +0700

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
+  
+  //để định danh thẻ cha chứa nó
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
+  
 }
